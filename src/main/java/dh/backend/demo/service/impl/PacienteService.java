c02c164b214ddc9b4eeab0e93bca19bb4d44592e
uma-dev <22565959+uma-dev@users.noreply.github.com>
2024-06-02 14:28:54 -0600

    agregar(service): implementacion del servicio de pacientes

--- src/main/java/dh/backend/demo/service/impl/PacienteService.java
+++ src/main/java/dh/backend/demo/service/impl/PacienteService.java
@@ -1,4 +1,42 @@
 package dh.backend.demo.service.impl;
 
-public class PacienteService {
+import dh.backend.demo.dao.IDao;
+import dh.backend.demo.model.Paciente;
+import dh.backend.demo.service.IPacienteService;
+import java.util.List;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PacienteService implements IPacienteService {
+
+  private IDao<Paciente> pacienteDao;
+
+  public PacienteService(IDao<Paciente> pacienteDao) {
+    this.pacienteDao = pacienteDao;
+  }
+
+  @Override
+  public Paciente registrarPaciente(Paciente paciente) {
+    return pacienteDao.registrar(paciente);
+  }
+
+  @Override
+  public Paciente buscarPorId(Integer id) {
+    return pacienteDao.buscarPorId(id);
+  }
+
+  @Override
+  public List<Paciente> buscarTodos() {
+    return pacienteDao.buscarTodos();
+  }
+
+  @Override
+  public void actualizarPaciente(Paciente paciente) {
+    pacienteDao.actualizar(paciente);
+  }
+
+  @Override
+  public void eliminarPaciente(Integer id) {
+    pacienteDao.eliminar(id);
+  }
 }
